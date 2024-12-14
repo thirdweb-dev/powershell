8b11165cf7c398fc9f0b0f185a0007232bcec75b
Nicholas St. Germain <nick@cajun.pro>
2024-12-13 18:07:51 -0600

    add internal update command

--- Modules/UnrealEngine/Helpers.psm1
+++ Modules/UnrealEngine/Helpers.psm1
@@ -78,3 +78,65 @@ function Start-Executable
         ExitCode = $exitCode
     }
 }
+
+function Update-ThirdwebModule
+{
+    $PSHomePath = Join-Path -Path $HOME -ChildPath "Documents\PowerShell"
+    $PSModulesPath = Join-Path -Path $PSHomePath -ChildPath "Modules"
+    $ThirdwebModuleDirectoryPath = Join-Path -Path $PSModulesPath -ChildPath "Thirdweb"
+
+    Write-Message "Checking for updates..."
+    $GitFetch = Start-Executable "git" `
+        -Arguments "fetch" `
+        -WorkingDirectory $ThirdwebModuleDirectoryPath
+
+    if (0 -ne $GitFetch.ExitCode)
+    {
+        throw "Failed to fetch status from github."
+    }
+
+    $GitStatus = Start-Executable "git" `
+        -Arguments "status" `
+        -WorkingDirectory $ThirdwebModuleDirectoryPath
+
+    if (0 -ne $GitStatus.ExitCode)
+    {
+        throw "Failed to read current module status"
+    }
+
+    elseif ($GitStatus.StdOut -match "Your branch is behind")
+    {
+        Write-Host "Updating Thirdweb PowerShell module..." -ForegroundColor Blue
+        $GitPull = Start-Executable "git" `
+            -Arguments "pull" `
+            -WorkingDirectory $ThirdwebModuleDirectoryPath
+
+        if (0 -ne $GitPull.ExitCode)
+        {
+            throw "Failed to update Thirdweb PowerShell module"
+        }
+        Write-Host "Thirdweb PowerShell module updated!" -ForegroundColor Green
+    }
+    elseif ($GitStatus.StdOut -match "up to date")
+    {
+        Write-Host "Thirdweb PowerShell module is already up to date" -ForegroundColor Blue
+    }
+    else
+    {
+        throw "Could not determine the current status of the module"
+    }
+
+    $ThirdwebModuleImportLines = @"
+# Thirdweb Module
+Import-Module '$ThirdwebModulePath'
+"@
+
+    $PSProfileContent = Get-Content -Path $PSProfilePath -Raw
+
+    if ($PSProfileContent -notlike "*# Thirdweb Module*")
+    {
+        Add-Content -Path $PSProfilePath -Value $ThirdwebModuleImportLines
+        Write-Host "Added Thirdweb Module to Profile via Import" -ForegroundColor Blue
+        Write-Host "To use, either close and reopen the terminal, or type . `$Profile" -ForegroundColor Blue
+    }
+}
